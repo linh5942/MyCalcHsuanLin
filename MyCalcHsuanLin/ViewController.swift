38fc47216bbfa42fee70ac2c6b3594371e70db04
linh5942 <linh594@newschool.edu>
2017-03-30 13:26:02 -0400

    created

--- MyCalcHsuanLin/ViewController.swift
+++ MyCalcHsuanLin/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
+enum modes{
+    case not_set
+    case addition
+}
+
 class ViewController: UIViewController {
 
+    var labelString:String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
+    
+    
+    @IBOutlet weak var CalcLabel: UILabel!
+    
+    
+    @IBAction func PressedNumber(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func PressedPlus(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func PressedClear(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func PressedEquals(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +49,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateText(){
+    }
+    
+    func changeMode(newMode:modes){
+    }
 
 }
 
